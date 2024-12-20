d18b3e0d181bcee922ae5a31ea7db2d21969242c
Ange230700 <kouakouangeericstephane@gmail.com>
2024-12-20 12:24:31 +0100

    feat: 🎸  tried something. I don't know what

---
+++ src/javascript/state/management.js
@@ -0,0 +1,16 @@
+const globalVariables = {
+  secret_code: ["blue", "red", "yellow", "green"],
+  attempts_number: 0,
+  colors_list: [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+    "black",
+  ],
+};
+
+export { globalVariables };
